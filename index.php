86540cfeea42359de9c910e31e0e155adf467947
Mateus Moura <chagas.mateus@gmail.com>
2015-10-28 00:33:04 -0200

    added button label in desktop version

--- index.php
+++ index.php
@@ -249,7 +249,9 @@
 
 								<div class="row">
 									<div class="col-12">
-										<button type="button" class="btn btn-blue btn-full">Filtrar</button>
+										<button type="button" class="btn btn-blue btn-full btn-filter">Filtrar</button>
+
+										<button type="button" class="btn btn-icon btn-blue btn-full btn-label">Gerar etiquetas <i class="icon icon-label"></i></button>
 									</div>
 								</div>
 							</div>
