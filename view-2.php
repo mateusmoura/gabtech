49d9a2e55de1f17f7eda142e01b05f8cfc1b2c28
Mateus Moura <chagas.mateus@gmail.com>
2015-11-07 19:20:22 -0200

    development view 2 versions mobile and desktop

---
+++ view-2.php
@@ -0,0 +1,269 @@
+<?php include("inc/header.php"); ?>
+
+			<main role="main" class="page__view">
+				<div class="page__header">
+					<div class="page__title">
+						<div class="center">
+							<div class="row">
+								<div class="col-10 col-m-9">
+									<a href="#this" class="block__menu--avatar">
+										<img src="img/fke/user-avatar.png">
+									</a>
+									<h2>Marcelo Oliveira da Costa</h2>
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<div class="center">
+						<div class="page__menu">
+							<a href="#" class="page__menu--item page__menu--item-active">Detalhes</a>
+							<a href="#" class="page__menu--item">Demandas</a>
+							<a href="#" class="page__menu--item">Utilidaes</a>
+							<a href="#" class="page__menu--item">Visitas</a>
+						</div>
+					</div>
+				</div>
+
+				<div class="page__content">
+					<div class="center">
+						<div class="block__view">
+							<div class="row">
+								<div class="col-12">
+									<div class="block__view--title borderline">
+										<div class="col-7 col-m-5">
+											<h3>Táxi</h3>
+										</div>
+
+										<div class="col-5 col-m-7 align-right">
+											<a href="#this" class="btn btn-blue btn-sm">Novo táxi</a>
+										</div>
+									</div>
+
+									<div class="block__panel">
+										<div class="block__panel--heading">
+											<div class="col-8">
+												<h4 class="block__panel--title"><i class="icon icon-car"></i> BarraTáxi</h4>
+											</div>
+
+											<div class="col-4 align-right">
+												<a href="#this" class="btn btn-link">Editar</a>
+											</div>
+										</div>
+
+										<div class="block__panel--body">
+											<div class="block__list">
+												<dl>
+													<dt>Telefone</dt>
+													<dd><a href="tel:+551133445566" class="block__number">(11) 3344-5566 <i class="icon icon-phone"></i></a></dd>
+													<dt>Telefone</dt>
+													<dd><a href="tel:+551133445566" class="block__number">(11) 3344-5566 <i class="icon icon-phone"></i></a></dd>
+													<dt>Email</dt>
+													<dd>barrataxi@gmail.com</dd>
+													<dt>Site</dt>
+													<dd><a href="#this">www.barrataxi.com.br</a></dd>
+												</dl>
+											</div>
+										</div>
+									</div>
+								</div>
+
+								<div class="col-12">
+									<div class="block__view--title borderline">
+										<div class="col-7 col-m-5">
+											<h3>Hotel</h3>
+										</div>
+
+										<div class="col-5 col-m-7 align-right">
+											<a href="#this" class="btn btn-blue btn-sm">Novo hotel</a>
+										</div>
+									</div>
+
+									<div class="block__panel">
+										<div class="block__panel--heading">
+											<div class="col-8">
+												<h4 class="block__panel--title"><i class="icon icon-hotel"></i> Carlton Hotel</h4>
+											</div>
+
+											<div class="col-4 align-right">
+												<a href="#this" class="btn btn-link">Editar</a>
+											</div>
+										</div>
+
+										<div class="block__panel--body">
+											<div class="block__list">
+												<dl>
+													<dt>Endereço</dt>
+													<dd>Setor Hoteleiro Sul - Q.5 - Bloco G </dd>
+													<dt>Telefone</dt>
+													<dd><a href="tel:+551133445566" class="block__number">(11) 3344-5566 <i class="icon icon-phone"></i></a></dd>
+													<dt>Email</dt>
+													<dd>reserva@carltonhotel.com.br</dd>
+													<dt>Site</dt>
+													<dd><a href="#this">www.carltonhotel.com.br</a></dd>
+												</dl>
+											</div>
+										</div>
+									</div>
+								</div>
+
+								<div class="col-12">
+									<div class="block__view--title borderline">
+										<div class="col-7 col-m-5">
+											<h3>Restaurante</h3>
+										</div>
+
+										<div class="col-5 col-m-7 align-right">
+											<a href="#this" class="btn btn-blue btn-sm">Novo restaurante</a>
+										</div>
+									</div>
+
+									<div class="block__panel">
+										<div class="block__panel--heading">
+											<div class="col-8">
+												<h4 class="block__panel--title"><i class="icon icon-restaurant"></i> Churrascaria Potência do Sul</h4>
+											</div>
+
+											<div class="col-4 align-right">
+												<a href="#this" class="btn btn-link">Editar</a>
+											</div>
+										</div>
+
+										<div class="block__panel--body">
+											<div class="block__list">
+												<dl>
+													<dt>Endereço</dt>
+													<dd>Setor Hoteleiro Sul - Q.5 - Bloco G </dd>
+													<dt>Telefone</dt>
+													<dd><a href="tel:+551133445566" class="block__number">(11) 3344-5566 <i class="icon icon-phone"></i></a></dd>
+													<dt>Email</dt>
+													<dd>reserva@carltonhotel.com.br</dd>
+													<dt>Site</dt>
+													<dd><a href="#this">www.carltonhotel.com.br</a></dd>
+												</dl>
+											</div>
+										</div>
+									</div>
+								</div>
+
+								<div class="col-12">
+									<div class="block__view--title borderline">
+										<div class="col-7 col-m-5">
+											<h3>Hospital</h3>
+										</div>
+
+										<div class="col-5 col-m-7 align-right">
+											<a href="#this" class="btn btn-blue btn-sm">Novo hospital</a>
+										</div>
+									</div>
+
+									<div class="block__panel">
+										<div class="block__panel--heading">
+											<div class="col-8">
+												<h4 class="block__panel--title"><i class="icon icon-hospital"></i> Hospital Santa Lúcia</h4>
+											</div>
+
+											<div class="col-4 align-right">
+												<a href="#this" class="btn btn-link">Editar</a>
+											</div>
+										</div>
+
+										<div class="block__panel--body">
+											<div class="block__list">
+												<dl>
+													<dt>Endereço</dt>
+													<dd>Setor Hoteleiro Sul - Q.5 - Bloco G </dd>
+													<dt>Telefone</dt>
+													<dd><a href="tel:+551133445566" class="block__number">(11) 3344-5566 <i class="icon icon-phone"></i></a></dd>
+													<dt>Email</dt>
+													<dd>reserva@carltonhotel.com.br</dd>
+													<dt>Site</dt>
+													<dd><a href="#this">www.carltonhotel.com.br</a></dd>
+												</dl>
+											</div>
+										</div>
+									</div>
+								</div>
+
+								<div class="col-12">
+									<div class="block__view--title borderline">
+										<div class="col-7 col-m-5">
+											<h3>Andamentos</h3>
+										</div>
+
+										<div class="col-5 col-m-7 align-right">
+											<a href="#this" class="btn btn-blue btn-sm">Novo andamento</a>
+										</div>
+									</div>
+
+									<div class="block__panel">
+										<div class="block__panel--heading">
+											<div class="col-8">
+												<h4 class="block__panel--title"><i class="icon icon-sheet"></i> 27/08/2015</h4>
+											</div>
+
+											<div class="col-4 align-right">
+												<a href="#this" class="btn btn-link">Editar</a>
+											</div>
+										</div>
+
+										<div class="block__panel--body">
+											<div class="block__list">
+												<dl>
+													<dt>Por</dt>
+													<dd><a href="#this">Oscar Moura</a></dd>
+													<dt>Situação</dt>
+													<dd>Deferido</dd>
+													<dt>Título</dt>
+													<dd>Conversar com o Prefeito sobre a nova obra da praça</dd>
+													<dt>Descrição</dt>
+													<dd>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum sed ut perspiciatis unde omnis iste natus.</dd>
+												</dl>
+											</div>
+										</div>
+									</div>
+								</div>
+
+								<div class="col-12">
+									<div class="block__view--title borderline">
+										<div class="col-7 col-m-5">
+											<h3>Lembretes</h3>
+										</div>
+
+										<div class="col-5 col-m-7 align-right">
+											<a href="#this" class="btn btn-blue btn-sm">Novo lembrete</a>
+										</div>
+									</div>
+
+									<div class="block__panel">
+										<div class="block__panel--heading">
+											<div class="col-8">
+												<h4 class="block__panel--title"><i class="icon icon-reminder"></i> 27/08/2015</h4>
+											</div>
+
+											<div class="col-4 align-right">
+												<a href="#this" class="btn btn-link">Editar</a>
+											</div>
+										</div>
+
+										<div class="block__panel--body">
+											<div class="block__list">
+												<dl>
+													<dt>Por</dt>
+													<dd><a href="#this">Oscar Moura</a></dd>
+													<dt>Título</dt>
+													<dd>Conversar com o Prefeito sobre a nova obra da praça</dd>
+													<dt>Descrição</dt>
+													<dd>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum sed ut perspiciatis unde omnis iste natus.</dd>
+												</dl>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</main>
+
+<?php include("inc/footer.php"); ?>
