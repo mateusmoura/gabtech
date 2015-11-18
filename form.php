189d0a1260fa2028d828319d1ad6c4790a4f62b8
Mateus Moura <chagas.mateus@gmail.com>
2015-11-18 00:39:40 -0200

    development form page.

---
+++ form.php
@@ -0,0 +1,349 @@
+<?php include("inc/header_form.php"); ?>
+
+			<main role="main" class="page__form">
+				<div class="page__form--header">
+					<div class="center">
+						<div class="row">
+							<div class="col-12">
+								<h2>Nova Demanda</h2>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div class="page__form--content">
+					<div class="center">
+						<div class="block__collapse block__collapse--open">
+							<div class="row">
+								<div class="col-12 col-m-4">
+									<div class="block__collapse--title">
+										<a href="#this"><i class="bullet bullet-num border-blue">1</i> Informações gerais <span class="btn-edit">Editar</span></a>
+
+										<div class="block__collapse--form-data">
+											<ul>
+												<li><p><strong>Tipo de recurso:</strong> Recurso</p></li>
+												<li><p><strong>Título:</strong> Construção da Quadra</p></li>
+												<li><p><strong>Data de entrada:</strong> 27/03/2015</p></li>
+												<li><p><strong>Data de entrada:</strong> 27/03/2015</p></li>
+											</ul>
+										</div>
+									</div>
+								</div>
+
+								<div class="col-12 col-m-8">
+									<div class="block__collapse--content">
+										<form class="validate" action="#" method="POST">
+											<div class="row">
+												<div class="col-12 col-m-3">
+													<label>Tipo de cadastro</label>
+												</div>
+
+												<div class="col-12 col-m-9">
+													<div class="radio">
+														<input type="radio" name="tipo" id="dado-recurso">
+														<label for="dado-recurso" class="sublabel">Recurso</label>
+													</div>
+
+													<div class="radio">
+														<input type="radio" name="tipo" id="dado-outros">
+														<label for="dado-outros" class="sublabel">Outros</label>
+													</div>
+												</div>
+											</div>
+
+											<div class="row">
+												<div class="col-12 col-m-3">
+													<label>Título <span>Campo obrigatório</span></label>
+												</div>
+
+												<div class="col-12 col-m-9">
+													<input type="text" class="form-style" placeholder="">
+												</div>
+											</div>
+
+											<div class="row">
+												<div class="col-12 col-m-3">
+													<label>Data de entrada</label>
+												</div>
+
+												<div class="col-12 col-m-3">
+													<div class="input-icon right">
+														<i class="icon icon-calendar"></i>
+														<input type="text" name="label26" id="dado-label26" class="form-style required mask-date datepicker" placeholder="Ex: 15/08/2015">
+													</div>
+												</div>
+											</div>
+
+											<div class="row">
+												<div class="col-12 col-m-3">
+													<label>Data Limite</label>
+												</div>
+
+												<div class="col-12 col-m-3">
+													<div class="input-icon right">
+														<i class="icon icon-calendar"></i>
+														<input type="text" name="label27" id="dado-label27" class="form-style required mask-date datepicker" placeholder="Ex: 15/08/2015">
+													</div>
+												</div>
+											</div>
+
+											<div class="row">
+												<div class="col-12">
+													<button type="submit" class="btn btn-blue btn-full btn-next"><i class="icon icon-arrow-bottom-b"></i>Continuar</button>
+												</div>
+											</div>
+										</form>
+									</div>
+								</div>
+							</div>
+						</div>
+
+						<div class="block__collapse">
+							<div class="row">
+								<div class="col-12">
+									<div class="block__collapse--title">
+										<a href="#this"><i class="bullet bullet-num border-blue">2</i> Tipo de demanda</a>
+									</div>
+								</div>
+
+								<div class="col-12">
+									<div class="block__collapse--content">
+										<form class="validate" action="#" method="POST">
+											<div class="row">
+												<div class="col-12">
+													<label>Tipo</label>
+												</div>
+
+												<div class="col-12">
+													<select name="label17" id="dado-label17" class="form-style">
+														<option value="">Selecione</option>
+														<option>Valor 1</option>
+														<option>Valor 2</option>
+														<option>Valor 3</option>
+													</select>
+
+													<a href="#this">+ Incluir novo tipo</a>
+												</div>
+											</div>
+
+											<div class="row">
+												<div class="col-12">
+													<label>Área de interesse</label>
+												</div>
+
+												<div class="col-12">
+													<select name="label18" id="dado-label18" class="form-style">
+														<option value="">Selecione</option>
+														<option>Valor 1</option>
+														<option>Valor 2</option>
+														<option>Valor 3</option>
+													</select>
+
+													<a href="#this">+ Incluir nova área de interesse</a>
+												</div>
+											</div>
+
+											<div class="row">
+												<div class="col-12">
+													<label>Orgão responsável</label>
+												</div>
+
+												<div class="col-12">
+													<select name="label19" id="dado-label19" class="form-style">
+														<option value="">Selecione</option>
+														<option>Valor 1</option>
+														<option>Valor 2</option>
+														<option>Valor 3</option>
+													</select>
+												</div>
+											</div>
+
+											<div class="row">
+												<div class="col-12">
+													<label>Finalidade do recurso</label>
+												</div>
+
+												<div class="col-12">
+													<div class="radio">
+														<input type="radio" name="tipo" id="dado-aquisicao">
+														<label for="dado-aquisicao" class="sublabel">Aquisição de bens</label>
+													</div>
+
+													<div class="radio">
+														<input type="radio" name="tipo" id="dado-projetos">
+														<label for="dado-projetos" class="sublabel">Projetos e obras</label>
+													</div>
+												</div>
+											</div>
+
+											<div class="row">
+												<div class="col-12">
+													<label>Valor do recurso</label>
+												</div>
+
+												<div class="col-12">
+													<input type="text" class="form-style" placeholder="">
+												</div>
+											</div>
+
+											<div class="row">
+												<div class="col-12">
+													<button type="submit" class="btn btn-blue btn-full btn-next"><i class="icon icon-arrow-bottom-b"></i>Continuar</button>
+												</div>
+											</div>
+										</form>
+									</div>
+								</div>
+							</div>
+						</div>
+
+						<div class="block__collapse">
+							<div class="row">
+								<div class="col-12">
+									<div class="block__collapse--title">
+										<a href="#this"><i class="bullet bullet-num border-blue">3</i> Solicitante</a>
+									</div>
+								</div>
+
+								<div class="col-12">
+									<div class="block__collapse--content">
+										<form class="validate" action="#" method="POST">
+											<div class="row">
+												<div class="col-12">
+													<label>Solicitante</label>
+												</div>
+
+												<div class="col-12">
+													<input type="text" class="form-style" placeholder="">
+
+													<a href="#this">+ Adicionar novo solicitante</a>
+												</div>
+											</div>
+
+											<div class="row">
+												<div class="col-12">
+													<label>Município</label>
+												</div>
+
+												<div class="col-12">
+													<input type="text" class="form-style" placeholder="">
+												</div>
+											</div>
+
+											<div class="row">
+												<div class="col-12">
+													<label>Interessado</label>
+												</div>
+
+												<div class="col-12">
+													<input type="text" class="form-style" placeholder="">
+
+													<a href="#this">+ Adicionar novo interessado</a>
+												</div>
+											</div>
+
+											<div class="row">
+												<div class="col-12">
+													<button type="submit" class="btn btn-blue btn-full btn-next"><i class="icon icon-arrow-bottom-b"></i>Continuar</button>
+												</div>
+											</div>
+										</form>
+									</div>
+								</div>
+							</div>
+						</div>
+
+						<div class="block__collapse">
+							<div class="row">
+								<div class="col-12">
+									<div class="block__collapse--title">
+										<a href="#this"><i class="bullet bullet-num border-blue">4</i> Informações adicionais</a>
+									</div>
+								</div>
+
+								<div class="col-12">
+									<div class="block__collapse--content">
+										<form class="validate" action="#" method="POST">
+											<div class="row">
+												<div class="col-12">
+													<label>Responsável</label>
+												</div>
+
+												<div class="col-12">
+													<input type="text" class="form-style" placeholder="">
+
+													<a href="#this">+ Adicionar outro responsável</a>
+												</div>
+											</div>
+
+											<div class="row">
+												<div class="col-12">
+													<label>Informações adicionais</label>
+												</div>
+
+												<div class="col-12">
+													<textarea class="form-style" col="5" row="10"></textarea>
+												</div>
+											</div>
+
+											<div class="row">
+												<div class="col-12">
+													<label>Anexos</label>
+												</div>
+
+												<div class="col-12">
+													<div class="block__list--archives">
+														<ul>
+															<li>
+																<div class="block__list--archives-item">
+																	<div class="col-2 col-m-3">
+																		<span class="block__list--archives-ext">DOC</span>
+																	</div>
+
+																	<div class="col-9 col-m-9">
+																		<p class="block__list--archives-name">nome-do-arquivo.doc</p>
+																	</div>
+
+																	<div class="col-1">
+																		<a href="#this" class="btn btn-link btn-icon btn-remove"><i class="icon icon-close-b"></i></a>
+																	</div>
+																</div>
+															</li>
+
+															<li>
+																<div class="block__list--archives-item">
+																	<div class="col-2 col-m-3">
+																		<span class="block__list--archives-ext">PDF</span>
+																	</div>
+
+																	<div class="col-9 col-m-9">
+																		<p class="block__list--archives-name">nome-do-arquivo-grande-tem-que-colocar-pontos-via-css</p>
+																	</div>
+
+																	<div class="col-1">
+																		<a href="#this" class="btn btn-link btn-icon btn-remove"><i class="icon icon-close-b"></i></a>
+																	</div>
+																</div>
+															</li>
+														</ul>
+													</div>
+
+													<a href="#this">+ Anexar novo arquivo</a>
+												</div>
+											</div>
+
+											<div class="row">
+												<div class="col-12">
+													<button type="submit" class="btn btn-blue btn-full btn-save"><i class="icon icon-check"></i>Salvar</button>
+												</div>
+											</div>
+										</form>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</main>
+
+<?php include("inc/footer.php"); ?>
---
+++ inc/header_form.php
@@ -0,0 +1,84 @@
+<!DOCTYPE HTML>
+<!--[if lt IE 7 ]> <html lang="pt-br" class="no-js lt-ie10 lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7 ]>    <html lang="pt-br" class="no-js lt-ie10 lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8 ]>    <html lang="pt-br" class="no-js lt-ie10 lt-ie9"> <![endif]-->
+<!--[if IE 9 ]>    <html lang="pt-br" class="no-js lt-ie10"> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!--> <html lang="pt-br" class="no-js"> <!--<![endif]-->
+	<head>
+		<meta charset="utf-8">
+		<title>NossoGabinete</title>
+		
+		<!-- meta -->
+		<link rel="apple-touch-icon" sizes="57x57" href="img/ico/apple-touch-icon-57x57.png">
+		<link rel="apple-touch-icon" sizes="60x60" href="img/ico/apple-touch-icon-60x60.png">
+		<link rel="apple-touch-icon" sizes="72x72" href="img/ico/apple-touch-icon-72x72.png">
+		<link rel="apple-touch-icon" sizes="76x76" href="img/ico/apple-touch-icon-76x76.png">
+		<link rel="apple-touch-icon" sizes="114x114" href="img/ico/apple-touch-icon-114x114.png">
+		<link rel="apple-touch-icon" sizes="120x120" href="img/ico/apple-touch-icon-120x120.png">
+		<link rel="apple-touch-icon" sizes="144x144" href="img/ico/apple-touch-icon-144x144.png">
+		<link rel="apple-touch-icon" sizes="152x152" href="img/ico/apple-touch-icon-152x152.png">
+		<link rel="apple-touch-icon" sizes="180x180" href="img/ico/apple-touch-icon-180x180.png">
+		<link rel="icon" type="image/png" href="img/ico/favicon-32x32.png" sizes="32x32">
+		<link rel="icon" type="image/png" href="img/ico/android-chrome-192x192.png" sizes="192x192">
+		<link rel="icon" type="image/png" href="img/ico/favicon-96x96.png" sizes="96x96">
+		<link rel="icon" type="image/png" href="img/ico/favicon-16x16.png" sizes="16x16">
+		<link rel="manifest" href="img/ico/manifest.json">
+		<link rel="mask-icon" href="img/ico/safari-pinned-tab.svg" color="#5bbad5">
+		<meta name="msapplication-TileColor" content="#da532c">
+		<meta name="msapplication-TileImage" content="/mstile-144x144.png">
+		<meta name="theme-color" content="#ffffff">
+
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<meta name="viewport" content="width=device-width,initial-scale=1.0">
+		<meta name='viewport' content='width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=0'>
+		<meta name="description" content="">
+		<meta name="author" content="https://br.linkedin.com/in/mateusmouraprofile" />
+		<link rel="shortcut icon" href="img/ico/favicon.ico" type="image/x-icon" />
+
+		<!-- PARA QUE A MEDIA QUERIES FUNCIONE NO IE8 -->
+		<!--[if lt IE 9]>
+		<script src="js/plugins/css3-mediaqueries.js"></script>
+		<![endif]-->
+
+		<link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+
+		<link rel="stylesheet" href="css/rwd-table.css">
+		<link rel="stylesheet" href="css/jQuery.ui-autocomplete.css">
+		<link rel="stylesheet" href="css/jQuery.ui-datepicker.css">
+		<link rel="stylesheet" href="css/base.css" media="screen" />
+		<!--[if IE]><link rel="stylesheet" href="css/ie.css" media="screen" /><![endif]-->
+
+		<!--[if lt IE 9]>
+		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+		<meta http-equiv="X-UA-Compatible" content="IE=EmulateIE9;">
+		<![endif]-->
+	</head>
+<!--[if !IE]>  -->
+	<body>
+<!-- <![endif]-->
+<!--[if IE 9]>
+	<body class="ie9">
+<![endif]-->
+<!--[if IE 10]>
+	<body class="ie10">
+<![endif]-->
+		<div id="global">
+			<header class="header">
+				<div class="center">
+					<div class="row">
+						<div class="col-6 col-m-8">
+							<h1 class="header__logo--form">
+								<a href="#this">
+									<i class="icon icon-clip"></i>
+									<i class="icon icon-clip-active"></i>
+									<img src="img/lgo/header__logo.svg" alt="NossoGabinete">
+								</a>
+							</h1>
+						</div>
+
+						<div class="col-6 col-m-4 align-right">
+							<button type="button" class="btn btn-link btn-icon btn-close-b"><i class="icon icon-close-b"></i></button>
+						</div>
+					</div>
+				</div>
+			</header>
