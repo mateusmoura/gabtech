e4124ec6cb09457ee9ffcfa75ce7c51b3f147467
Mateus Moura <chagas.mateus@gmail.com>
2015-11-29 13:37:39 -0200

    :hammer: development modal default 1. Version mobile and desktop

--- inc/modal-default-1.php
+++ inc/modal-default-1.php
@@ -14,40 +14,44 @@
 			<div class="modal__content">
 				<div class="row">
 					<div class="col-12 align-center col-m-8 central">
-						<h1><strong>238</strong> destinatários válidos</h1>
+						<div class="modal__content--intro">
+							<h1><strong>238</strong> destinatários válidos</h1>
 
-						<p>Dos 266 contatos selecionados, 28 estão com o cadastro incompleto e serão desconsiderados para evitar etiquetas em branco.</p>
+							<p>Dos 266 contatos selecionados, 28 estão com o cadastro incompleto e serão desconsiderados para evitar etiquetas em branco.</p>
+						</div>
 					</div>
 				</div>
 
 				<div class="row">
 					<div class="col-12 col-m-6 central">
-						<span>Deseja gerar etiquetas de  remetente?</span>
+						<div class="modal__content--form">
+							<span>Deseja gerar etiquetas de  remetente?</span>
 
-						<form action="">
-							<div class="block__list--bordered">
-								<div class="block__list--bordered-item">
-									<div class="radio">
-										<input type="radio" name="etiqueta" id="dado-tipo1" checked="checked">
-										<label for="dado-tipo1" class="sublabel">Não, somente destinatário.</label>
+							<form action="">
+								<div class="block__list--bordered">
+									<div class="block__list--bordered-item">
+										<div class="radio">
+											<input type="radio" name="etiqueta" id="dado-tipo1" checked="checked">
+											<label for="dado-tipo1" class="sublabel">Não, somente destinatário.</label>
+										</div>
+									</div>
+									<div class="block__list--bordered-item">
+										<div class="radio">
+											<input type="radio" name="etiqueta" id="dado-tipo2" checked="checked">
+											<label for="dado-tipo2" class="sublabel">Gabinete de Brasília</label>
+										</div>
+									</div>
+									<div class="block__list--bordered-item">
+										<div class="radio">
+											<input type="radio" name="etiqueta" id="dado-tipo3" checked="checked">
+											<label for="dado-tipo3" class="sublabel">Gabinete de Curitiba</label>
+										</div>
 									</div>
 								</div>
-								<div class="block__list--bordered-item">
-									<div class="radio">
-										<input type="radio" name="etiqueta" id="dado-tipo2" checked="checked">
-										<label for="dado-tipo2" class="sublabel">Gabinete de Brasília</label>
-									</div>
-								</div>
-								<div class="block__list--bordered-item">
-									<div class="radio">
-										<input type="radio" name="etiqueta" id="dado-tipo3" checked="checked">
-										<label for="dado-tipo3" class="sublabel">Gabinete de Curitiba</label>
-									</div>
-								</div>
-							</div>
 
-							<button type="submit" class="btn btn-success btn-full">Gerar etiquetas</button>
-						</form>
+								<button type="submit" class="btn btn-success btn-full">Gerar etiquetas</button>
+							</form>
+						</div>
 					</div>
 				</div>
 			</div>
