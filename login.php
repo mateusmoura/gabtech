950bf89ab3b66ef7131ef3ca250f008ebd62c03e
Mateus Moura <chagas.mateus@gmail.com>
2015-12-14 21:37:02 -0200

    :bug: fix login page.

--- login.php
+++ login.php
@@ -2,17 +2,38 @@
 <html lang="pt-br">
 	<head>
 		<meta charset="utf-8">
-		<title>Gab Tech &raquo; Lorem</title>
+		<title>NossoGabinete</title>
 		
 		<!-- meta -->
+		<link rel="apple-touch-icon" sizes="57x57" href="img/ico/apple-touch-icon-57x57.png">
+		<link rel="apple-touch-icon" sizes="60x60" href="img/ico/apple-touch-icon-60x60.png">
+		<link rel="apple-touch-icon" sizes="72x72" href="img/ico/apple-touch-icon-72x72.png">
+		<link rel="apple-touch-icon" sizes="76x76" href="img/ico/apple-touch-icon-76x76.png">
+		<link rel="apple-touch-icon" sizes="114x114" href="img/ico/apple-touch-icon-114x114.png">
+		<link rel="apple-touch-icon" sizes="120x120" href="img/ico/apple-touch-icon-120x120.png">
+		<link rel="apple-touch-icon" sizes="144x144" href="img/ico/apple-touch-icon-144x144.png">
+		<link rel="apple-touch-icon" sizes="152x152" href="img/ico/apple-touch-icon-152x152.png">
+		<link rel="apple-touch-icon" sizes="180x180" href="img/ico/apple-touch-icon-180x180.png">
+		<link rel="icon" type="image/png" href="img/ico/favicon-32x32.png" sizes="32x32">
+		<link rel="icon" type="image/png" href="img/ico/android-chrome-192x192.png" sizes="192x192">
+		<link rel="icon" type="image/png" href="img/ico/favicon-96x96.png" sizes="96x96">
+		<link rel="icon" type="image/png" href="img/ico/favicon-16x16.png" sizes="16x16">
+		<link rel="manifest" href="img/ico/manifest.json">
+		<link rel="mask-icon" href="img/ico/safari-pinned-tab.svg" color="#5bbad5">
+		<meta name="msapplication-TileColor" content="#da532c">
+		<meta name="msapplication-TileImage" content="/mstile-144x144.png">
+		<meta name="theme-color" content="#ffffff">
+
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<meta name="viewport" content="width=device-width,initial-scale=1.0">
+		<meta name='viewport' content='width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=0'>
 		<meta name="description" content="">
-		<meta name="author" content="" />
+		<meta name="author" content="https://br.linkedin.com/in/mateusmouraprofile" />
 		<link rel="shortcut icon" href="img/ico/favicon.ico" type="image/x-icon" />
 
 		<!-- PARA QUE A MEDIA QUERIES FUNCIONE NO IE8 -->
 		<!--[if lt IE 9]>
-		<script src="js/plugins/respond.js"></script>
+		<script src="js/plugins/css3-mediaqueries.js"></script>
 		<![endif]-->
 
 		<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700' rel='stylesheet' type='text/css'>
